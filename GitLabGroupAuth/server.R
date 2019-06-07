00291a538ff2cf44d6e9a959128552e04ec64a22
Raymond Canchola <Raymond@Raymonds-MacBook-Pro.local>
2019-06-07 12:04:31 -0400

    Group Authenication

--- GitLabGroupAuth/server.R
+++ GitLabGroupAuth/server.R
@@ -1,9 +1,9 @@
 source('global.R')
 shinyServer(function(input, output, session) {
   
-  ####################
-  #User Authenication#
-  ####################
+  #####################
+  #User Authentication#
+  #####################
   
   #Reading the url from user
   #Documentation: https://shiny.rstudio.com/articles/client-data.html
@@ -21,11 +21,11 @@ shinyServer(function(input, output, session) {
                                           grant_type = "authorization_code",
                                           client_secret = client_secret))
   #Gitlab sends a json array containing all needed information such as access_token, scope etc.
-  #From Here we will begin our group authenication
+  #From Here we will begin our group authentication
   
-  #####################
-  #Group Authenication#
-  #####################
+  ######################
+  #Group Authentication#
+  ######################
   
   #Step 1: Getting Access token from gitlab's response
   
@@ -34,7 +34,7 @@ shinyServer(function(input, output, session) {
   
   #Step 2: Connecting api
   
-  #Connecting api to gitlab to verify if the user is in the group with access_token recieved from the 'user authenication' process.
+  #Connecting api to gitlab to verify if the user is in the group with access_token recieved from the 'User Authentication' process.
   #A high level understanding is that this step goes to your company's gitlab repository and checks if the user is in the correct group by using groups?search.
   #Documentation: https://docs.gitlab.com/ee/api/groups.html
   
@@ -45,10 +45,10 @@ shinyServer(function(input, output, session) {
   #Step 3: Ui page determintion based on gitlab's group search
   
   observe({
-    #If user has credientials, then the user gets redirected to your dashboard. If not, the user gets sent to a page
+    #If the user has credientials, then the user gets redirected to your dashboard. If not, the user gets sent to a page
     #asking them to contact the adminstrator of the app to include them into the gitlab's group list.
     
-    if (length(group) == 0){ #If user is not in group then the length of group will be 0 because the response from gitlab will be empty
+    if (length(group) == 0){ #If the user is not in group then the length of group will be 0 because the response from gitlab will be empty.
       shinyjs::hide(id = "login-page")
       shinyjs::show(id = "request-page")
     }
