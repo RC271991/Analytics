dbc9ad8b01acfacd29b7547008e2ca33b6feb1ee
RC271991 <51482132+RC271991@users.noreply.github.com>
2020-03-16 15:55:44 -0400

    Update LDA.R

--- Algorithms/Supervised Learning Algorithms/LDA.R
+++ Algorithms/Supervised Learning Algorithms/LDA.R
@@ -1,5 +1,8 @@
 #For reference: https://www.youtube.com/watch?v=OaIIr29Pj2g and https://web.stanford.edu/class/stats202/content/lec9.pdf
 ## Linear Discriminant Anlaysis
+list.of.packages <- c("plotly", "matrixcalc", "dplyr")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 
 library(matrixcalc)#used for matrix inverse
 library(plotly)
