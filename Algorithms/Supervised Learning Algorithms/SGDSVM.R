d2fcaa719f2ef1ed4789e8fa3832c08f52708587
RC271991 <51482132+RC271991@users.noreply.github.com>
2020-03-16 16:00:23 -0400

    Update SGDSVM.R

--- Algorithms/Supervised Learning Algorithms/SGDSVM.R
+++ Algorithms/Supervised Learning Algorithms/SGDSVM.R
@@ -1,4 +1,10 @@
 #SGD on linear seperable data
+
+# Installing Packages
+list.of.packages <- c("plotly", "matrixcalc", "dplyr")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
 #Hard-margin
 library(plotly)
 
